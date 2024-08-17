08fb3ee6c18b59c585c660ed4a1446bae127dcb6
Oleh Purys <wenger1901@gmail.com>
2024-08-17 20:53:52 +0300

    try

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,34 +1,33 @@
-// import Swiper from './libraries';
 import { Navigation, Keyboard, Swiper } from './libraries.js';
+import 'swiper/css';
+import 'swiper/css/autoplay';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
-document.addEventListener('DOMContentLoaded', onSwiper);
-
-function onSwiper() {
-  const swiper = new Swiper('.swiper', {
-    modules: [Navigation, Keyboard],
-    direction: 'horizontal',
-    watchOverflow: false,
-    speed: 700,
-    spaceBetween: 16,
-    breakpoints: {
-      320: {
-        slidesPerView: 1,
-      },
-      768: {
-        slidesPerView: 2,
-      },
-      1440: {
-        slidesPerView: 4,
-      },
+const swiper = new Swiper('.swiper', {
+  modules: [Navigation, Keyboard],
+  direction: 'horizontal',
+  watchOverflow: false,
+  speed: 700,
+  spaceBetween: 16,
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
     },
-
-    keyboard: {
-      enabled: true,
+    768: {
+      slidesPerView: 2,
     },
-
-    navigation: {
-      nextEl: '.reviews-btn-next',
-      prevEl: '.reviews-btn-prev',
+    1440: {
+      slidesPerView: 4,
     },
-  });
-}
+  },
+
+  keyboard: {
+    enabled: true,
+  },
+
+  navigation: {
+    nextEl: '.reviews-btn-next',
+    prevEl: '.reviews-btn-prev',
+  },
+});
