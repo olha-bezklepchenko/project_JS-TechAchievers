27c8c297155631c62822af7309515797f8852d17
Oleh Purys <wenger1901@gmail.com>
2024-08-17 20:48:12 +0300

    try to fix swiper

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,30 +1,34 @@
 // import Swiper from './libraries';
 import { Navigation, Keyboard, Swiper } from './libraries.js';
 
-export const swiper = new Swiper('.swiper', {
-  modules: [Navigation, Keyboard],
-  direction: 'horizontal',
-  watchOverflow: false,
-  speed: 700,
-  spaceBetween: 16,
-  breakpoints: {
-    320: {
-      slidesPerView: 1,
-    },
-    768: {
-      slidesPerView: 2,
-    },
-    1440: {
-      slidesPerView: 4,
-    },
-  },
+document.addEventListener('DOMContentLoaded', onSwiper);
 
-  keyboard: {
-    enabled: true,
-  },
+function onSwiper() {
+  const swiper = new Swiper('.swiper', {
+    modules: [Navigation, Keyboard],
+    direction: 'horizontal',
+    watchOverflow: false,
+    speed: 700,
+    spaceBetween: 16,
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1440: {
+        slidesPerView: 4,
+      },
+    },
 
-  navigation: {
-    nextEl: '.reviews-btn-next',
-    prevEl: '.reviews-btn-prev',
-  },
-});
+    keyboard: {
+      enabled: true,
+    },
+
+    navigation: {
+      nextEl: '.reviews-btn-next',
+      prevEl: '.reviews-btn-prev',
+    },
+  });
+}
